0ba6f48002103c229aa8d76b1c97dfd9a93f4aed
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2024-07-23 19:21:01 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
 import { getData } from "@/sanity/sanity-utils";
-import urlFor from "@/lib/urlBuilder";
 
+interface Pottery {
+  _id: string;
+  title: string;
+  image: string;
+  imageAlt: string;
+  description: string;
+}
 
 
 export default async function Home() {
@@ -13,7 +19,7 @@ export default async function Home() {
       <h1>Mormor och Jag</h1>
 
      <ul>
-        {data.map((pottery) => (
+        {data.map((pottery:Pottery) => (
           <li key={pottery._id}>
             <h2>{pottery.title}</h2>
             <Image src={pottery.image} alt={pottery.imageAlt} width={200} height={200} />
