1f92604673ce955669182ddbcf69d6e83d020b13
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2025-04-21 18:52:03 +0200

    Update pageLinks.ts

--- lib/pageLinks.ts
+++ lib/pageLinks.ts
@@ -1,25 +1,24 @@
-
 const pageLinks = [
-    {
-      title: 'Hem',
-      path: '/',
-    },    
-    {
-      title: 'Galleri',
-      path: '/gallery',
-    },
-    {
-      title: 'Butik',
-      path: '/store',
-    },
-    {
+  {
+    title: 'Hem',
+    path: '/',
+  },
+  {
+    title: 'Galleri',
+    path: '/gallery',
+  },
+  {
+    title: 'Butik',
+    path: '/store',
+  },
+  /*  {
       title: 'Blogg',
       path: '/blog',
-    },    
-    {
-        title: 'Kontakt',
-        path: '/contact',
-    },
-  ]
-  
-  export default pageLinks
+    },     */
+  {
+    title: 'Kontakt',
+    path: '/contact',
+  },
+]
+
+export default pageLinks
